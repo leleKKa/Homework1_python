4a5b1e56306dd50d469d8dcb153fe1a544969e08
Olga <oadotik@gmail.com>
2023-05-28 13:00:25 +0300

    сохранение изменений

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+n = 'dfdf\'jhhhj'
+# print(n)
+
+a = 5
+b = 5.89
+c = 'hello'
+
+print (a,'-', b,c)
+print(f"{a} - {b} - {c}")
+print("{} - {} - {}".format(a,b,c))
+"""
+
+#print("Введите первое число: ")
+#a = input()
+#print(a)
+# b = input("Введите второе число: ")
+#print(a, ' + ', b, ' = ', a+b)
+
+c = 5.89
+print(c)
+print(type(c))
+c = int(c)
+print(type(c))
